681278feb3d77bfae0c5f318fcbda707345af1e8
unknown <olena.denysova88@gmail.com>
2021-08-19 13:28:00 +0200

    slider and flip

--- script.js
+++ script.js
@@ -10,3 +10,100 @@ const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
 
+// Keep track of current card
+let currentActiveCard = 0;
+
+//Store DOM cards
+const cardsEl = [];
+
+//Store card data
+ const cardsData = [
+   {
+     question: 'What must a variable begin with?',
+     answer: 'A letter, $ or _'
+   },
+   {
+     question: 'What is HTML?',
+     answer: 'HyperText Markup Language is the standard markup language for documents designed to be displayed in a web browser'
+   },
+   {
+     question: ' What is CSS?',
+     answer: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML'
+   }
+ ];
+
+ // Create all cards
+function createCards() {
+  cardsData.forEach((data, index) => createCard(data, index));
+}
+
+// Create a single card in DOM
+function createCard(data, index) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  if (index === 0) {
+    card.classList.add('active');
+  }
+
+  card.innerHTML = `
+  <div class="inner-card">
+  <div class="inner-card-front">
+    <p>
+      ${data.question}
+    </p>
+  </div>
+  <div class="inner-card-back">
+    <p>
+      ${data.answer}
+    </p>
+  </div>
+</div>
+  `;
+
+  card.addEventListener('click', () => card.classList.toggle('show-answer'));
+
+  // Add to DOM cards
+  cardsEl.push(card);
+
+  cardsContainer.appendChild(card);
+
+  updateCurrentText();
+}
+
+// Show number of cards
+function updateCurrentText() {
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
+
+createCards();
+
+// Next button
+nextBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card left';
+
+  currentActiveCard = currentActiveCard + 1;
+
+  if (currentActiveCard > cardsEl.length - 1) {
+    currentActiveCard = cardsEl.length - 1;
+  }
+
+  cardsEl[currentActiveCard].className = 'card active';
+
+  updateCurrentText();
+});
+
+// Prev button
+prevBtn.addEventListener('click', () => {
+  cardsEl[currentActiveCard].className = 'card right';
+
+  currentActiveCard = currentActiveCard - 1;
+
+  if (currentActiveCard < 0) {
+    currentActiveCard = 0;
+  }
+
+  cardsEl[currentActiveCard].className = 'card active';
+
+  updateCurrentText();
+});
