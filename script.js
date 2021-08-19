2b7165bdad6d2590c5acad7754e5478111cb0763
unknown <olena.denysova88@gmail.com>
2021-08-19 14:40:25 +0200

    script

--- script.js
+++ script.js
@@ -9,15 +9,19 @@ const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
+const error = document.getElementById('error');
 
 // Keep track of current card
 let currentActiveCard = 0;
+
+// Store card data
+const cardsData = getCardsData();
 
 //Store DOM cards
 const cardsEl = [];
 
 //Store card data
- const cardsData = [
+ /* const cardsData = [
    {
      question: 'What must a variable begin with?',
      answer: 'A letter, $ or _'
@@ -30,7 +34,7 @@ const cardsEl = [];
      question: ' What is CSS?',
      answer: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML'
    }
- ];
+ ];  */
 
  // Create all cards
 function createCards() {
@@ -76,6 +80,32 @@ function updateCurrentText() {
   currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
+// Show number of cards
+function updateCurrentText() {
+  currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
+
+// Get cards from local storage
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem('cards'));
+  return cards === null ? [
+    {
+    question: 'What is HTML?',
+    answer: 'HyperText Markup Language is the standard markup language for documents designed to be displayed in a web browser'
+    },
+    {
+      question: 'What must a variable begin with?',
+      answer: 'A letter, $ or _'
+    }
+  ] : cards;
+}
+
+// Add card to local storage
+function setCardsData(cards) {
+  localStorage.setItem('cards', JSON.stringify(cards));
+  window.location.reload();
+}
+
 createCards();
 
 // Next button
@@ -85,7 +115,7 @@ nextBtn.addEventListener('click', () => {
   currentActiveCard = currentActiveCard + 1;
 
   if (currentActiveCard > cardsEl.length - 1) {
-    currentActiveCard = cardsEl.length - 1;
+    currentActiveCard = 0;
   }
 
   cardsEl[currentActiveCard].className = 'card active';
@@ -100,10 +130,44 @@ prevBtn.addEventListener('click', () => {
   currentActiveCard = currentActiveCard - 1;
 
   if (currentActiveCard < 0) {
-    currentActiveCard = 0;
+    currentActiveCard = cardsEl.length - 1;
   }
 
   cardsEl[currentActiveCard].className = 'card active';
 
   updateCurrentText();
+});
+
+// Show add container
+showBtn.addEventListener('click', () => addContainer.classList.add('show'));
+// Hide add container
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+
+// Add new card
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer };
+
+    createCard(newCard);
+
+    questionEl.value = '';
+    answerEl.value = '';
+
+    addContainer.classList.remove('show');
+
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  } else {
+    error.style.display = 'block';
+  }
+});
+
+// Clear cards button
+clearBtn.addEventListener('click', () => {
+  localStorage.clear();
+  cardsContainer.innerHTML = '';
+  window.location.reload();
 });
